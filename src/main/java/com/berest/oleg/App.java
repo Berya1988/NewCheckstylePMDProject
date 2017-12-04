d220ed52f613e254207ef2aedd90d750b1fab62b
olbe0615 <oleg.berest@NetCracker.com>
2017-12-04 17:32:20 +0200

    Initial commit

---
+++ src/main/java/com/berest/oleg/App.java
@@ -0,0 +1,15 @@
+package com.berest.oleg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void zbc(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
